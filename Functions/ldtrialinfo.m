59981112d0364c4c339d4965a3e375e7071c2736
Mohammad Rabiei <47454187+MRabieiGh@users.noreply.github.com>
2022-08-26 22:06:27 +0430

    did some modifications

--- Functions/ldtrialinfo.m
+++ Functions/ldtrialinfo.m
@@ -4,11 +4,22 @@ function [A1, A2, B1, B2, prob, trialType] = ldtrialinfo(cmat, trial)
 %   of condition matrix we are at (trial), then parses the matrix and
 %   returns options' weights (A1, A2, B1, B2) and chart's probability and
 %   trialtype (solo or info)
-A1          = cmat(trial, 2);
-A2          = cmat(trial, 3);
-B1          = cmat(trial, 4);
-B2          = cmat(trial, 5);
-prob        = cmat(trial, 6);
-trialType   = cmat(trial, 7);
+
+safeOption  = cmat(trial, 8);
+trialType = cmat(trial, 7);
+
+if ((trialType==2) || (trialType==4)) && safeOption == 2
+    B1 = cmat(trial, 2);
+    B2 = cmat(trial, 3);
+    A1 = cmat(trial, 4);
+    A2 = cmat(trial, 5);
+else
+    A1 = cmat(trial, 2);
+    A2 = cmat(trial, 3);
+    B1 = cmat(trial, 4);
+    B2 = cmat(trial, 5);
+end
+
+prob = cmat(trial, 6);
 end
 
